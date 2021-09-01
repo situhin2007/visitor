bf8fcf7315132cf9a8488274d90adaaba919826a
ripon <riponahmed2201@gmail.com>
2021-09-02 01:19:40 +0600

    menu fix

--- resources/views/backend/includes/navbar.blade.php
+++ resources/views/backend/includes/navbar.blade.php
@@ -4,6 +4,19 @@
       <li class="nav-item">
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
+
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="{{ route('dashboard') }}" class="nav-link">Patient</a>
+      </li>
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="javascript:void(0);" class="nav-link">Doctor</a>
+      </li>
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="javascript:void(0);" class="nav-link">Management</a>
+      </li>
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="javascript:void(0);" class="nav-link">Vendor</a>
+      </li>
     </ul>
 
     <!-- Right navbar links -->
