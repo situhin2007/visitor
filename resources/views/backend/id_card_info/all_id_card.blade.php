781aa94213fd5f7f2597e537611dc69135abf0c9
ripon <riponahmed2201@gmail.com>
2021-10-31 00:12:30 +0600

    issue fix

--- resources/views/backend/id_card_info/all_id_card.blade.php
+++ resources/views/backend/id_card_info/all_id_card.blade.php
@@ -12,7 +12,7 @@
 <div class="row">
     @foreach ($id_cards as $value)
         <div class="col-lg-2 col-6">
-            <div style="width: 200px" class="small-box @if ($value->status == 0) bg-success @elseif ($value->status == 1) bg-info @elseif ($value->status == 2) bg-warning  @elseif ($value->status == 3) bg-danger @else @endif">
+            <div style="width: 200px" class="small-box @if ($value->status == 0) bg-success @elseif ($value->status == 1) bg-info @elseif ($value->status == 2) bg-danger @else @endif">
             <div class="inner">
                 <h3>{{ $value->id_card_no }}</h3>
                <a href="{{ url('id_card_generate/edit',$value->id) }}" class="text-white"> <p>Card Number</p></a>
