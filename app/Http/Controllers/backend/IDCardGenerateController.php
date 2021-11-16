43551cb056bbd4299913884e91d487146dc719cd
Md. Ripon <ripon@pop-os.localdomain>
2021-11-16 23:12:02 +0600

    id card issue fix

--- app/Http/Controllers/backend/IDCardGenerateController.php
+++ app/Http/Controllers/backend/IDCardGenerateController.php
@@ -11,7 +11,7 @@ class IDCardGenerateController extends Controller
     public function index()
     {
         $id_cards = IdCardGenerate::all();
-        return view('backend.id_card_info.all_id_card',compact('id_cards'));
+        return view('backend.id_card_info.all_id_card', compact('id_cards'));
     }
 
     public function create()
@@ -22,25 +22,34 @@ class IDCardGenerateController extends Controller
     public function store(Request $request)
     {
         $this->validate($request, [
-            'id_card_no' => 'required',
-            'serial_no' => 'required',
+            'from_id_card' => 'required',
+            'to_id_card' => 'required',
         ]);
 
-        $id_card_generate = new IdCardGenerate();
-        $id_card_generate->id_card_no = $request->id_card_no;
-        $id_card_generate->serial_no = $request->serial_no;
-
-        if ($id_card_generate->save()) {
-            return redirect('id_card_generate/index')->with('success', 'Id number successfully saved.');
-        } else {
-            return back()->with('error', 'Something Error Found, Please try again');
+        for ($x = $request->from_id_card; $x <= $request->to_id_card; $x++) {
+            // echo "The number is: $x <br>";
+            $id_card_generate = new IdCardGenerate();
+            $id_card_generate->id_card_no = $x;
+            $id_card_generate->save();
         }
+
+        // $id_card_generate = new IdCardGenerate();
+        // $id_card_generate->id_card_no = $request->id_card_no;
+        // $id_card_generate->serial_no = $request->serial_no;
+
+        return redirect('id_card_generate/index')->with('success', 'Id number successfully saved.');
+
+        // if ($id_card_generate->save()) {
+        //     return redirect('id_card_generate/index')->with('success', 'Id number successfully saved.');
+        // } else {
+        //     return back()->with('error', 'Something Error Found, Please try again');
+        // }
     }
 
     public function edit($id)
     {
         $id_card = IdCardGenerate::findOrFail($id);
-        return view('backend.id_card_info.edit',compact('id_card'));
+        return view('backend.id_card_info.edit', compact('id_card'));
     }
 
     public function update(Request $request, $id)
--- resources/views/backend/id_card_info/create.blade.php
+++ resources/views/backend/id_card_info/create.blade.php
@@ -6,54 +6,54 @@
 @endsection
 
 @section('content')
-<section class="content">
-    <div class="container-fluid">
-      <!-- SELECT2 EXAMPLE -->
-      <div class="card card-default">
-        <div class="card-header">
-          <h3 class="card-title">Add New</h3>
-          <a href="{{ url('id_card_generate/index') }}" class="btn btn-primary btn-sm float-right"> <i class="fa fa-table"></i> All ID Number</a>
+    <section class="content">
+        <div class="container-fluid">
+            <!-- SELECT2 EXAMPLE -->
+            <div class="card card-default">
+                <div class="card-header">
+                    <h3 class="card-title">Add New</h3>
+                    <a href="{{ url('id_card_generate/index') }}" class="btn btn-primary btn-sm float-right"> <i
+                            class="fa fa-table"></i> All ID Number</a>
 
-        </div>
-        <!-- /.card-header -->
-        @include('backend.includes.message')
-
-        <form action="{{ url('id_card_generate/store') }}" method="POST">
-            @csrf
-        <div class="card-body">
-          <div class="row">
-
-            <div class="col-md-6">
-              <div class="form-group">
-                <label>ID Number <span class="text-danger">*</span></label>
-                <input type="text" class="form-control" name="id_card_no" placeholder="Enter New ID No. ">
-                @if($errors->has('id_card_no'))
-                    <span class="text-danger">{{ $errors->first('id_card_no') }}</span>
-                @endif
-              </div>
-            </div>
-
-            <div class="col-md-6">
-                <div class="form-group">
-                  <label>Serial Number <span class="text-danger">*</span></label>
-                  <input type="text" class="form-control" name="serial_no" placeholder="Enter Serial No. (ex: chip no.)">
-                @if($errors->has('serial_no'))
-                    <span class="text-danger">{{ $errors->first('serial_no') }}</span>
-                @endif
                 </div>
-              </div>
+                <!-- /.card-header -->
+                @include('backend.includes.message')
 
-          </div>
+                <form action="{{ url('id_card_generate/store') }}" method="POST">
+                    @csrf
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label>From <span class="text-danger">*</span></label>
+                                    <input type="text" class="form-control" name="from_id_card" placeholder="From">
+                                    @if ($errors->has('from_id_card'))
+                                        <span class="text-danger">{{ $errors->first('from_id_card') }}</span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label>To <span class="text-danger">*</span></label>
+                                    <input type="text" class="form-control" name="to_id_card" placeholder="To">
+                                    @if ($errors->has('to_id_card'))
+                                        <span class="text-danger">{{ $errors->first('to_id_card') }}</span>
+                                    @endif
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                    <!-- /.card-body -->
+                    <div class="card-footer">
+                        <button type="submit" class="btn btn-success">Submit</button>
+                    </div>
+                </form>
+            </div>
         </div>
-        <!-- /.card-body -->
-        <div class="card-footer">
-            <button type="submit" class="btn btn-success">Submit</button>
-        </div>
-    </form>
-      </div>
-    </div>
-    <!-- /.container-fluid -->
-  </section>
+        <!-- /.container-fluid -->
+    </section>
 @endsection
 
 
